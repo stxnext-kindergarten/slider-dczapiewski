e5227a5d9dc933b16f446b4d014e1e90cfc6b826
damianc <damianczapiewski@gmail.com>
2015-10-06 15:28:19 +0200

    Docs added. First version working

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,25 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     minifyCss = require('gulp-minify-css');
 
+/**
+ * Default action when gulp is run with no task.
+ * Displays welcome message.
+ */
 gulp.task('default', function() {
     gutil.log('==', gutil.colors.green('Slider'), 'jQuery Plugin', '==');
 });
 
+/**
+ * Copies files from /src directory to /dist directory
+ */
 gulp.task('dev', function() {
     gulp.src('src/**')
     .pipe(gulp.dest('dist'));
 });
 
+/**
+ * Minifies JavaScript and CSS files.
+ */
 gulp.task('prod', function() {
     gulp.src('dist/js/*.js')
     .pipe(uglify())
